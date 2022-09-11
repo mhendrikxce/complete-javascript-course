f32063a56cff49166d22fe9e7d14fe2fec3e9c0d
mhendrikx89 <mark_hendrikx_1@hotmail.com>
2022-09-11 11:27:34 +0200

    11. Practice Assignments

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,4 @@
+let text = 'Welcome in alert';
+alert(text);
+console.log(text + ' and in the console')
+
---
+++ practice assignments/10. Values and Variables/script.js
@@ -0,0 +1,9 @@
+// let text = 'Welcome in alert';
+// alert(text);
+// console.log(text + ' and in the console')
+
+let country = 'The Netherlands';
+let continent = 'Europe';
+let population = 17;
+
+console.log(country + "\n" + continent + "\n" + population);
